1ee4d99830cb3be3523ad0cc5c203261b9d88bd0
PentiumRabbit <zo.ru.yg@gmail.com>
2015-06-26 19:00:09 +0800

    添加升级接口

---
+++ app/src/main/java/com/storm/powerimprove/bean/UpdateItem.java
@@ -0,0 +1,9 @@
+package com.storm.powerimprove.bean;
+
+/**
+ * @author ----zhaoruyang----
+ * @data: 2015/6/26
+ */
+public class UpdateItem {
+    private static final String TAG = UpdateItem.class.getSimpleName();
+}
