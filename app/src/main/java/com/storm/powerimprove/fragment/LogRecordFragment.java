e5e5b5535ee59f8104dc525da776af059aebc496
PentiumRabbit <zo.ru.yg@gmail.com>
2015-07-22 16:51:36 +0800

    修改工具界面

--- app/src/main/java/com/storm/powerimprove/fragment/LogRecordFragment.java
+++ app/src/main/java/com/storm/powerimprove/fragment/LogRecordFragment.java
@@ -2,11 +2,17 @@ package com.storm.powerimprove.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.widget.AppCompatSpinner;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.storm.powerimprove.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Log操作界面
@@ -16,6 +22,12 @@ import com.storm.powerimprove.R;
  */
 public class LogRecordFragment extends LocalDialogFragment {
     private static final String TAG = LogRecordFragment.class.getSimpleName();
+    @InjectView(R.id.til_name)
+    TextInputLayout tilName;
+    @InjectView(R.id.sp_log_type)
+    AppCompatSpinner spLogType;
+    @InjectView(R.id.Button)
+    android.widget.Button Button;
 
     /**
      * Returns a new instance of this fragment for the given section
@@ -29,6 +41,19 @@ public class LogRecordFragment extends LocalDialogFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_log_record, container, false);
+        View view = inflater.inflate(R.layout.fragment_log_record, container, false);
+        ButterKnife.inject(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
     }
 }
