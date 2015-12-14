1588b1dbb9808993e48983e94dbb912e299a1c18
PentiumRabbit <zo.ru.yg@gmail.com>
2015-12-15 00:18:44 +0800

    完善下观察者

--- AndroidCommon/src/com/android/base/common/Observer/ObserverType.java
+++ AndroidCommon/src/com/android/base/common/Observer/ObserverType.java
@@ -8,11 +8,12 @@ package com.android.base.common.Observer;
  * @author ----zhaoruyang----
  * @data: 2015/1/9
  */
-public enum ObserverType {
+public class ObserverType {
+    public static int base = 0;
     /*网络状态类型*/
-    NET_STATUS,
+    public static final int NET_STATUS = base++;
     /*闹铃时钟*/
-    ALARM_NOTIFY,
+    public static final int ALARM_NOTIFY = base++;
     /*私有协议变化*/
-    PROTOCOL_NOTIFY
+    public static final int PROTOCOL_NOTIFY = base++;
 }
