98ecd170b6dc73fb43833b05eb31cc8c7a4158c0
zhaoruyang <zhaoruyang@meituan.com>
2018-04-25 11:50:29 +0800

    backup

---
+++ AndroidCommon/src/com/android/base/common/view/stickydecoration/listener/GroupListener.java
@@ -0,0 +1,5 @@
+package com.android.base.common.view.stickydecoration.listener;
+
+public interface GroupListener {
+    String getGroupName(int position);
+}
---
+++ AndroidCommon/src/com/android/base/common/view/stickydecoration/listener/OnGroupClickListener.java
@@ -0,0 +1,5 @@
+package com.android.base.common.view.stickydecoration.listener;
+
+public interface OnGroupClickListener {
+    void onClick(int position, int id);
+}
