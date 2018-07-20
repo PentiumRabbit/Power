9431ce2d7f91bce503c09383eb57ac648c659a28
zhaoruyang <zhaoruyang@meituan.com>
2018-07-20 19:52:51 +0800

    backup

---
+++ CommonModule/src/com/android/base/common/view/group/GroupInfo.java
@@ -0,0 +1,36 @@
+package com.android.base.common.view.group;
+
+/**
+ * @author : ZhaoRuYang
+ * @date : 2018/7/17
+ */
+public class GroupInfo {
+    private int type;
+    private String color;
+
+    public int getType() {
+        return type;
+    }
+
+    public GroupInfo setType(int type) {
+        this.type = type;
+        return this;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public GroupInfo setColor(String color) {
+        this.color = color;
+        return this;
+    }
+
+    public interface Type {
+        int all = 1;
+        int top = 2;
+        int mid = 3;
+        int bottom = 4;
+    }
+
+}
